16999ddea1207be251a22f9e35d97f83f27a43a7
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-01-26 20:37:17 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import Styles from './styles.module.scss'
+
+export function Header() {
+    return (
+        <header className={Styles.headerContainer}>
+            <div className={Styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <a className={Styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
