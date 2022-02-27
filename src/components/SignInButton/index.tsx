15fc84b8fef895f6ef3dd8af87d0f901d2933f7c
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-02-27 10:21:37 -0300

    neth-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,24 +2,30 @@ import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
+    const {
+        data: session
+    } = useSession()
 
-    const isUserLogged = true;
+    console.log(session)
 
-    return isUserLogged ? (
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
-            <FaGithub color='##04D361' />
-            Pablo Vicente
+            <FaGithub color='#04D361' />
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#EBA417' />
             Sign in with GitHub
