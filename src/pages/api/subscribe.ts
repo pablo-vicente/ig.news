3597fed7dc2062796f04002ce2a561315092a64b
Pablo Vicente <pablo.vicente@outlook.com.br>
2022-03-04 19:46:53 -0300

    hadle duplicate user

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,21 +1,57 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { query as q } from "faunadb";
+import { faunadb } from "../../services/faunadb";
 import { stripe } from "../../services/stipe";
+
+type User = {
+    ref: {
+        id: string
+    },
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (req.method === 'POST') {
 
         const session = await getSession({ req })
+        const user = await faunadb.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            // name: session.user.name,
-        })
+        let costumerId = user.data.stripe_customer_id
+
+        if (!costumerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                // name: session.user.name,
+            })
+
+            await faunadb.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            costumerId = stripeCustomer.id
+        }
 
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: costumerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -29,9 +65,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             success_url: process.env.STRIPE_SUCCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
-
-        console.log("stripeCheckoutSession")
-        console.log(stripeCheckoutSession)
 
         return res.status(200).json({
             sessionId: stripeCheckoutSession.id
